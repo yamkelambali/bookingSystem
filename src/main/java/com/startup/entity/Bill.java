de3426ebff19ea0828c47caa9a7f8efe83ef3f9e
Yamkelambali <Yamkelambaliym@gmail.com>
2020-07-04 13:09:16 +0200

    First commit

---
+++ src/main/java/com/startup/entity/Bill.java
@@ -0,0 +1,78 @@
+package com.startup.entity;
+
+public class Bill {
+
+    private String billNo, appointId, patientId, amount;
+
+    private Bill(Builder builder){
+        this.billNo = builder.billNo;
+        this.appointId = builder.appointId;
+        this.patientId = builder.patientId;
+        this.amount = builder.amount;
+    }
+
+    public String getBillNo() {
+        return billNo;
+    }
+
+    public String getAppointId() {
+        return appointId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Bill{" +
+                "billNo='" + billNo + '\'' +
+                ", appointId='" + appointId + '\'' +
+                ", patientId='" + patientId + '\'' +
+                ", amount='" + amount + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String billNo, appointId, patientId, amount;
+
+        public Builder setBillNo(String billNo){
+
+            this.billNo = billNo;
+            return this;
+        }
+
+        public Builder setAppointId(String appointId){
+
+            this.appointId = appointId;
+            return this;
+        }
+
+        public Builder setPatientId(String patientId){
+
+            this.patientId = patientId;
+            return this;
+        }
+        public Builder setAmount(String amount){
+
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder copy(Bill bill){
+            this.billNo = bill.billNo;
+            this.appointId = bill.appointId;
+            this.patientId = bill.patientId;
+            this.amount = bill.amount;
+            return this;
+        }
+
+        public Bill build(){
+            return new Bill (this);
+        }
+    }
+}
---
+++ src/main/java/com/startup/entity/User.java
@@ -0,0 +1,124 @@
+package com.startup.entity;
+
+public class User {
+
+    private String username, password, userId, name, surname, cellNo, emailAddress, loginStatus;
+
+    private User(Builder builder){
+        this.username = builder.username;
+        this.password = builder.password;
+        this.userId = builder.userId;
+        this.name = builder.name;
+        this.surname = builder.name;
+        this.cellNo = builder.cellNo;
+        this.emailAddress = builder.emailAddress;
+        this.loginStatus = builder.loginStatus;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getCellNo() {
+        return cellNo;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", cellNo='" + cellNo + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", loginStatus='" + loginStatus + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String username, password, userId, name, surname, cellNo, emailAddress, loginStatus;
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder setSurName(String surname) {
+            this.surname = surname;
+            return this;
+        }
+        public Builder setUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setCellNo(String cellNo) {
+            this.cellNo = cellNo;
+            return this;
+        }
+
+        public Builder setEmailAddress(String emailAddress) {
+            this.emailAddress = emailAddress;
+            return this;
+        }
+        public Builder setLoginStatus(String loginStatus) {
+            this.loginStatus = loginStatus;
+            return this;
+        }
+
+        public Builder copy(User user){
+            this.username = user.username;
+            this.password = user.password;
+            this.name = user.name;
+            this.surname = user.surname;
+            this.userId = user.userId;
+            this.cellNo = user.cellNo;
+            this.emailAddress = user.emailAddress;
+            this.loginStatus = user.loginStatus;
+
+            return this;
+        }
+
+        public User build(){
+            return new User(this);
+        }
+
+    }
+
+}
