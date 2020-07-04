c9012b443d2e24e3d62f98ff1558f514e62d6c6a
JackAnati <Anatijack0@gmail.com>
2020-07-04 21:34:16 +0200

    Commit Patient and Receptionist

--- src/main/java/com/startup/entity/Patient.java
+++ src/main/java/com/startup/entity/Patient.java
@@ -1,4 +1,64 @@
 package com.startup.entity;
 
 public class Patient {
+
+    private String patientId, historyReport, medicalAidId;
+
+    private Patient(Builder builder){
+        this.patientId = builder.patientId;
+        this.historyReport = builder.historyReport;
+        this.medicalAidId = builder.medicalAidId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getHistoryReport() {
+        return historyReport;
+    }
+
+    public String getMedicalAidId() {
+        return medicalAidId;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientId='" + patientId + '\'' +
+                ", historyReport='" + historyReport + '\'' +
+                ", medicalAidId='" + medicalAidId + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private  String patientId, historyReport, medicalAidId;
+
+        public Builder setPatientId(String patientId){
+            this.patientId = patientId;
+            return this;
+        }
+
+        public Builder setHistoryReport(String historyReport){
+            this.historyReport = historyReport;
+            return this;
+        }
+
+        public Builder setMedicalAidId(String medicalAidId){
+            this.medicalAidId = medicalAidId;
+            return this;
+        }
+
+        public Patient.Builder copy(Patient patient){
+            this.patientId = patient.patientId;
+            this.historyReport = patient.historyReport;
+            this.medicalAidId = patient.medicalAidId;
+            return this;
+        }
+
+        public Patient build(){
+            return new Patient (this);
+        }
+    }
+
 }
