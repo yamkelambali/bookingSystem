1450e7d1c6601d59e9aadc988fcb82a593f039f4
Yamkelambali <Yamkelambaliym@gmail.com>
2020-07-05 21:23:14 +0200

    User Factory

---
+++ src/main/java/com/startup/factory/UserFactory.java
@@ -0,0 +1,28 @@
+package com.startup.factory;
+
+import com.startup.entity.User;
+import com.startup.util.GenericHelper;
+
+import javax.print.DocFlavor;
+
+/**
+ * @author Yamkela Mbali
+ * desc : factory classs for User
+ */
+
+public class UserFactory {
+
+    public static User createUser(String username, String password, String name, String surname, String cellNo, String emailAddress ){
+
+        String userId = GenericHelper.generateID();
+        User user = new User.Builder().setUserId(userId)
+                .setUsername(username)
+                .setPassword(password)
+                .setName(name)
+                .setSurName(surname)
+                .setCellNo(cellNo)
+                .setEmailAddress(emailAddress)
+                .build();
+        return null;
+    }
+}
