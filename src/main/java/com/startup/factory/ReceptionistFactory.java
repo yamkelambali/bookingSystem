07dbf17bbf1205f211afbe5ca695506d9354c8a9
Yamkelambali <Yamkelambaliym@gmail.com>
2020-08-31 04:21:02 +0200

    Revert "Account and Bill Entities Added"
    This reverts commit 0bfd2b16ccf2aa1cf3b86d6bdbe59fa3c33f3b3a.

--- src/main/java/com/startup/factory/ReceptionistFactory.java
+++ src/main/java/com/startup/factory/ReceptionistFactory.java
@@ -7,12 +7,7 @@ import com.startup.util.GenericHelper;
  */
 
 public class ReceptionistFactory {
-    public static Receptionist createReceptionist(String qualification){
-        String receptionistID = GenericHelper.generateId();
-        Receptionist receptionist = new Receptionist.Builder()
-                .setAdminId(receptionistID)
-                .setQualification(qualification)
-                .build();
+
         return receptionist;
     }
 }
