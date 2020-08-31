709a14eeb25fb7105482cae9abd02153f3456d2b
Michael Bezuidenhout <61207961+217037240@users.noreply.github.com>
2020-08-31 02:04:05 +0200

    Add files via upload

--- src/test/java/com/startup/Repository/impl/BillRepositoryImplTest.java
+++ src/test/java/com/startup/Repository/impl/BillRepositoryImplTest.java
@@ -7,6 +7,8 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import static org.junit.Assert.*;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BillRepositoryImplTest {
 
