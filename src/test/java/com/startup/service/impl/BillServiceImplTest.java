c1d783d1bf2d83bed440ea3545873c461748db4c
JackAnati <Anatijack0@gmail.com>
2020-09-06 18:35:05 +0200

    Bill Updated

---
+++ src/test/java/com/startup/service/impl/BillServiceImplTest.java
@@ -0,0 +1,60 @@
+package com.startup.service.impl;
+
+import com.startup.entity.Bill;
+import com.startup.entity.Receptionist;
+import com.startup.factory.BillFactory;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * @author Anati Jack
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class BillServiceImplTest {
+
+    private static BillService service = BillServiceImpl.getBill();
+    private static Bill bill = BillFactory.generateBill("120456", "54778888", "1500");
+
+
+    @Test
+    public void d_getAll() {
+        Set<Bill>bills = service.getAll();
+        assertEquals(1, bills.size());
+        System.out.println("All: " + bills);
+    }
+
+    @Test
+    public void a_create() {
+        Bill created = service.create(bill);
+        Assert.assertEquals(bill.getBillNo(), created.getBillNo());
+        System.out.println("Create bill:  " + created);
+    }
+
+    @Test
+    public void b_read() {
+        Bill read = service.read(bill.getBillNo());
+        System.out.println("Read bill:  " + read);
+    }
+
+    @Test
+    public void c_update() {
+        Bill updated = new Bill.Builder().copy(bill).setAppointId("125485").setPatientId("542136582").setAmount("R399.00").build();
+        updated = service.update(updated);
+        System.out.println("Update Bill:  " + updated);
+    }
+
+    @Test
+    public void e_delete() {
+        boolean deleted = service.delete(bill.getBillNo());
+        Assert.assertTrue(deleted);
+    }
+
+
+}
