03c7c3d75ca793ba8ad2f6dbaf980c9cfec1ae10
Yamkelambali <Yamkelambaliym@gmail.com>
2020-10-27 17:13:55 +0200

    DB 2

--- src/main/java/com/startup/Repository/impl/AccountRepository.java
+++ src/main/java/com/startup/Repository/impl/AccountRepository.java
@@ -2,9 +2,10 @@ package com.startup.Repository.impl;
 
 import com.startup.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
-
+@Repository
 public interface AccountRepository extends JpaRepository<Account, String>{
 
 }
--- src/main/java/com/startup/Repository/impl/UserRepository.java
+++ src/main/java/com/startup/Repository/impl/UserRepository.java
@@ -4,9 +4,10 @@ package com.startup.Repository.impl;
  */
 import com.startup.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, String>{
 
 }
